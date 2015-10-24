65233fbcd10bde52205521dc2657a7b3b03feff3
Jisoo Han <jisooh@wepay.com>
2015-10-24 01:39:08 -0700

    Hardcode database credentials

--- eLocal_proj/settings.py
+++ eLocal_proj/settings.py
@@ -56,6 +56,17 @@ ROOT_URLCONF = 'eLocal_proj.urls'
 
 WSGI_APPLICATION = 'eLocal_proj.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd96iocm5fsitfn',
+        'USER': 'dxyqecmwszokaa',
+        'PASSWORD': '0vAibn-sA4f69zf-eehe1Vz2rh',
+        'HOST': 'ec2-107-21-219-142.compute-1.amazonaws.com', # Or something like this
+        'PORT': '5432',
+    }
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -72,22 +83,6 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd96iocm5fsitfn',
-        'USER': 'dxyqecmwszokaa',
-        'PASSWORD': '0vAibn-sA4f69zf-eehe1Vz2rh',
-        'HOST': 'ec2-107-21-219-142.compute-1.amazonaws.com', # Or something like this
-        'PORT': '5432',
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -103,13 +98,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'eLocal_app/static'),
 )
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config()

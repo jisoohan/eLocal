b98c7381e1cfb2a4e858b9e276d694beb263f325
Jisoo Han <jisooh@wepay.com>
2015-10-23 21:56:36 -0700

    ui tests

---
+++ eLocal_app/tests/ui_tests.py
@@ -0,0 +1,29 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class UITest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def testHomePage(self):
+        driver = self.driver
+        driver.get('http://localhost:8000')
+        self.assertIn('eLocal', driver.title)
+
+    def testProductPage(self):
+        driver = self.driver
+        driver.get('http://localhost:8000/products')
+        self.assertIn('Product', driver.title)
+
+    def testStorePage(self):
+        driver = self.driver
+        driver.get('http://localhost:8000/stores')
+        self.assertIn('Store', driver.title)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == '__main__':
+    unittest.main()

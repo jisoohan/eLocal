4a634142339b299b59c55f41bf07809ddf0f1f36
Alex Ahmed <alexahmed@berkeley.edu>
2015-11-25 15:56:06 -0700

    Added check_distance test for utils.py

--- eLocal_app/utils.py
+++ eLocal_app/utils.py
@@ -7,10 +7,12 @@ def json_response(response_dict, status=200):
     response = HttpResponse(json.dumps(response_dict), content_type="application/json", status=status)
     response['Access-Control-Allow-Origin'] = '*'
     response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    #print(response.content_type)
     return response
 
 def check_distance(origin, destination, radius):
+    print(origin)
+    print(destination)
+    print(radius)
     lat1 = deg2rad(float(origin[0]))
     lon1 = deg2rad(float(origin[1]))
     lat2 = deg2rad(float(destination[0]))
@@ -18,6 +20,7 @@ def check_distance(origin, destination, radius):
     r = 3958.8
 
     distance = 2*3958.8*asin(sqrt( haversin(lat2-lat1) + cos(lat1)*cos(lat2)*haversin(lon2-lon1) ))
+    print(distance <= radius)
     return distance <= radius
 
 def haversin(theta):
